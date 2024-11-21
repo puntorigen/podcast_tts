2f9c170cf5b946c8ceae0b521116b8dfc9b68cfc
Pablo Schaffner <pablo@puntorigen.com>
2024-11-20 21:15:57 -0300

    - updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="podcast_tts",
-    version="0.0.1",
+    version="0.0.2",
     description="Generate high-quality TTS audio for podcasts and dialogues.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
